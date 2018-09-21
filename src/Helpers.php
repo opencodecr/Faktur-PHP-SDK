f66d4dc9794f8313b9999f1d46353954e259fa47
Open Code 506 <opencode506@gmail.com>
2018-09-20 21:59:00 -0600

    Se elimina metodo que no se va a utilizar

--- src/Helpers.php
+++ src/Helpers.php
@@ -16,30 +16,13 @@ class Helpers {
         $header_text = substr($response, 0, strpos($response, "\r\n\r\n"));
 
         foreach (explode("\r\n", $header_text) as $i => $line)
-            if ($i === 0) return $headers['http_code'] = $line;
+            if ($i === 0) 
+                return $headers['http_code'] = $line;
             
             list ($key, $value) = explode(': ', $line);
             $headers[$key] = $value;
             
         return $headers;
-    }
-
-    public function ping($domain)
-    {
-        $starttime = microtime(true);
-        $file      = fsockopen ($domain, 80, $errno, $errstr, 10);
-        $stoptime  = microtime(true);
-        $status    = 0;
-
-        if (!$file) {
-            // Sito offline
-            $status = false;
-        } else {
-            fclose($file);
-            $status = ($stoptime - $starttime) * 1000;
-            $status = floor($status);
-        }
-        return $status;
     }
 
 }
